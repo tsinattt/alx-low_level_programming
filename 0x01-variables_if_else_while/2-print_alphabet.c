cb9f8e9998bfcf1d475dd2ad88fe2122e89035ca
tsinattt <tsinatk@gmail.com>
2022-06-11 01:19:35 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - program that prints the alphabet in lowercase, followed by a new line.
+ * You can only use the putchar function is forbidden
+ * You can only use putchar twice in your code
+ * Return: 0
+ */
+int main(void)
+{
+int l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+l += 1;
+}
+putchar('\n');
+return (0);
+}
