6362a864d351c1e429fdf7fae3644e78f3e64679
tsinattt <tsinatk@gmail.com>
2022-06-10 19:19:29 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints outa string stdout. 
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
