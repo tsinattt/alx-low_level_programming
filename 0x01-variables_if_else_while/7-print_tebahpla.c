ed3fe9dab18f10a7888f7f8b3648fce1c8dac4c3
tsinattt <tsinatk@gmail.com>
2022-06-12 04:02:28 -0700

    The last digit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: Tsinat Kifle Abebaw
+ */
+
+#include <stdio.h>
+
+/*
+ * main - program that prints the lowercase alphabet in reverse, followed by a
+ * You can only use the putchar function
+ * All your code should be in the main function
+ * You can only use putchar twice in your code
+ * Return: 0
+ */
+
+int main (void)
+{
+	charr letter;
+	
+	for (letter = 'z';letter >= 'a';letter--)
+	putchar(letter);
+	
+	putchar('\n');
+	
+	return (0);
+}
