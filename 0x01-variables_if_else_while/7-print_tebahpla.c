af301c11aa6f1a0217183888837d2aec00ae364f
tsinattt <tsinatk@gmail.com>
2022-06-12 04:16:08 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -15,12 +15,12 @@
 
 int main (void)
 {
-	charr letter;
-	
-	for (letter = 'z';letter >= 'a';letter--)
-	putchar(letter);
-	
-	putchar('\n');
-	
-	return (0);
+char letter;
+
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+
+putchar('\n');
+
+return (0);
 }
