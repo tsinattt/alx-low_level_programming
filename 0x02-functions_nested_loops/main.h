d7fa7e129f4e7ba91a9997640b77152c2333f3aa
tsinattt <tsinatk@gmail.com>
2022-06-16 05:24:20 -0700

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-fndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -14,4 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
-#endif /* _MAIN_H_ */
+#endif
