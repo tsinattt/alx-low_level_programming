2fdb747c89430f01154c16ab92e4fe09b68717fc
tsinattt <tsinatk@gmail.com>
2023-09-07 01:43:49 -0700

    100

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
- * main - Main function
- *
- * Return: return 0
- */
-int main() {
-  printf("Programming is like building a multilingual puzzle.\n");
-  return 0;
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
 }
