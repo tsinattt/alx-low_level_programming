c583f286fff42796d8f32cfe5be01324f0affe42
tsinattt <tsinatk@gmail.com>
2023-09-07 01:24:16 -0700

    6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 
-/*
- *main - Main function of project
+/**
+ * main - Main function
  *
  * Return: return 0
  */
-int main(void)
-{
-	char displayed[52];
-
-	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-	puts(displayed);
-	return (0);
+int main() {
+  printf("Programming is like building a multilingual puzzle.\n");
+  return 0;
 }
