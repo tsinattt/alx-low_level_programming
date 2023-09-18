573f3dcaa1f5c9947c2b1d0ab7eaea427b6b1e28
tsinattt <tsinatk@gmail.com>
2023-09-18 04:04:09 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * puts2 - prints every character of a string
  *
  * @str: string parameter input
  *
- * Return: Nothing
- */
+ * Return: Noting
+*/
 
 void puts2(char *str)
 {
@@ -14,8 +14,8 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; ++i)
 	{
-		if  (i % 2 == 0)
-			_putchar(str[str]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
