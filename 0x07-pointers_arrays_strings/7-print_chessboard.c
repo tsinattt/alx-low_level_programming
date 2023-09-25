79d54d0925b4346c54b65b8b13ad19eeecc54e78
tsinattt <tsinatk@gmail.com>
2023-09-25 02:47:26 -0700

    7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,23 +1,18 @@
 #include "main.h"
 
 /**
- * _strchr - Locates a character in a string.
- * @s: The string to be searched.
- * @c: The character to be located.
- *
- * Return: If c is found - a pointer to the first occurence.
- * If c is not found - NULL.
+ * print_chessboard - Prints a chessboard.
+ * @a: The chessboard to be printed.
  */
-
-char *_strchr(char *s, char c)
+void print_chessboard(char (*a)[8])
 {
-	int index;
+	int indx1, indx2;
 
-	for (index = 0; s[index] >= '\0'; index++)
+	for (indx1 = 0; a[indx1][7]; indx1++)
 	{
-		if (s[index] == c)
-			return (s + index);
-	}
+		for (indx2 = 0; indx2 < 8; indx2++)
+			_putchar(a[indx1][indx2]);
 
-	return ('\0');
+		_putchar('\n');
+	}
 }
